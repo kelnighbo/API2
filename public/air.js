ba2f34358b7c467d62a7a880228b3b463ebda675
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2020-10-03 16:46:58 -0400

    commit

--- public/air.js
+++ public/air.js
@@ -1,4 +1,4 @@
-const baseURl = 'http://api.airvisual.com/v2/city?';
+const baseURl = 'https://api.airvisual.com/v2/city?';
 const key = 'c3606135-b947-439f-8e8b-57ce6253a66a';
 const button = document.getElementById('fetchbutton');
 button.addEventListener('click', fetchData)
@@ -15,13 +15,20 @@ function displayData(jsonified) {
     console.log(jsonified);
     let hu=document.getElementById("hu")
     humidity = jsonified.data.current.weather.hu;
-    hu.innerHTML = (`Humidity: ${humidity}`);
+    hu.innerHTML = hu.innerHTML+ (`${humidity}`);
     let pr=document.getElementById("pr")
     precipitation = jsonified.data.current.weather.pr
-    pr.innerHTML = (`Precipitation: ${precipitation}`)
+    pr.innerHTML = pr.innerHTML+ (`${precipitation}`)
     let ws=document.getElementById("ws")
     windshear = jsonified.data.current.weather.ws;
-    ws.innerHTML = (`Wind Shear: ${windshear}`)
+    ws.innerHTML = ws.innerHTML+ (`${windshear}`)
+    let aq=document.getElementById("aq")
+    let aqicn = jsonified.data.current.pollution.aqicn;
+    aq.innerHTML = aq.innerHTML+ (`${aqicn}`);
+    let aqius=document.getElementById("aqius")
+    let aqiusData = jsonified.data.current.pollution.aqius;
+    aqius.innerHTML = aqius.innerHTML+ (`${aqiusData}`);
 }
+
 
 
